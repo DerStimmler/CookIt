d85a17f04f20ad2a6601224c1e1e63bb726a48b9
Paula Sollner <paula.sollner@gmx.de>
2018-10-19 11:38:08 +0200

    Active der Navbar ändert sich bei Click & ganzes Li-Feld ist jetzt anklickbar

--- src/index.js
+++ src/index.js
@@ -9,4 +9,25 @@ window.addEventListener("load", () => {
   // Anwendung starten
   let app = new App();
   app.start();
+
+  document.getElementById("search").addEventListener("click",function(){
+    document.getElementById("search").classList.add('active');
+    document.getElementById("meine").classList.remove('active');
+    document.getElementById("inf").classList.remove('active');
+  }
+  );
+
+  document.getElementById("inf").addEventListener("click",function(){
+    document.getElementById("inf").classList.add('active');
+    document.getElementById("meine").classList.remove('active');
+    document.getElementById("search").classList.remove('active');
+}
+);
+
+    document.getElementById("meine").addEventListener("click",function(){
+        document.getElementById("meine").classList.add('active');
+        document.getElementById("inf").classList.remove('active');
+        document.getElementById("search").classList.remove('active');
+  }
+  );
 });
